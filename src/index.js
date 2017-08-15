788d87169f08f443c029e440b0109385aac6e2f3
Adham EL Banhawy <the.benhawy@gmail.com>
2017-08-14 22:48:23 -0500

    Added props

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import SkiDayCount from './SkiDayCount';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<SkiDayCount total={50}
+                             powder={20}
+                             backcountry={10}
+                             goal={100} />, document.getElementById('root'));
 registerServiceWorker();
