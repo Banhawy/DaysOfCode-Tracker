a35623b49b941eb8d2fa5186ee7801a74c604fa4
Adham EL Banhawy <the.benhawy@gmail.com>
2017-08-14 23:25:40 -0500

    Added React-icons

--- src/SkiDayCount.js
+++ src/SkiDayCount.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
+import Terrain from 'react-icons/lib/md/terrain';
+import SnowFlake from 'react-icons/lib/ti/weather-snow';
+import Calendar from 'react-icons/lib/fa/calendar';  
 
 class SkiDayCount extends Component {
   percentToDecimal(decimal){
@@ -17,14 +20,17 @@ class SkiDayCount extends Component {
       <div className="ski-day-count">
         <div className="total-days">
           <span>{this.props.total}</span>
+            <Calendar /> 
           <span> days</span>
         </div>
         <div className="powder-days">
-        <span>{this.props.powder}</span>
+          <span>{this.props.powder}</span>
+            <SnowFlake /> 
           <span> days</span>
         </div>
         <div className="backcountry-days">
-        <span>{this.props.backcountry}</span>
+          <span>{this.props.backcountry}</span>
+            <Terrain /> 
           <span> days</span>
         </div>
         <div className="">
