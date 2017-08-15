16b330fe3c018c1b4bd543a7a1856f99bf520088
Adham EL Banhawy <the.benhawy@gmail.com>
2017-08-15 02:26:08 -0500

    Added react routers

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Router, Route, hashHistory } from 'react-router';
 
 // ReactDOM.render(<SkiDayCount backcountry={200} />, document.getElementById('root'));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Router history={hashHistory}>
+                    <Route path="/" component={App}/>
+                    <Route path="*" component={Whoops404}/>
+                </Router>, document.getElementById('root'));
 registerServiceWorker();
