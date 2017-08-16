e43cd9c4ea9223c3eb5e9707a24c872eb4cfd35d
Adham EL Banhawy <the.benhawy@gmail.com>
2017-08-15 22:49:14 -0500

    Changed to stateless component

--- src/AddDayForm.js
+++ src/AddDayForm.js
@@ -1,50 +1,49 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class AddDayForm extends Component {
-    constructor (props) {
-        super(props)
-        this.submit = this.submit.bind(this)
-    }
-    
-    submit(e){
+const AddDayForm = ({resort, date, powder, backcountry, onNewDay }) => {
+    let _resort, _date, _powder, _backcountry
+    const submit = (e) => {
+        onNewDay({
+            resort: _resort,
+            date: _date,
+            powder: _powder,
+            backcountry: _backcountry
+        })
         e.preventDefault()
-        console.log("resort", this.refs.resort.value)
-        console.log("date", this.refs.date.value)
-        console.log("powder", this.refs.powder.checked)
-        console.log("backcountry", this.refs.backcountry.checked)
+        _resort = ""
+        _date = ""
+        _powder = false
+        _backcountry = false
     }
-    render () {
-        const {resort, date, powder, backcountry} = this.props 
-        return (
-            <div>
-                <form onSubmit={this.submit} className="add-day-form">
-                    <label htmlFor="resort">Resort Name</label>
-                    <input id="form" 
-                           type="text" 
-                           required
-                           defaultValue={resort}
-                           ref="resort" />
+    return (
+        <div>
+            <form onSubmit={submit} className="add-day-form">
+                <label htmlFor="resort">Resort Name</label>
+                <input id="form" 
+                       type="text" 
+                       required
+                       defaultValue={resort}
+                       ref={input => _resort = input} />
 
-                    <label htmlFor="date">Date</label>
-                    <input id="date" 
-                           type="date" 
-                           required
-                           defaultValue={date}
-                           ref="date" />
+                <label htmlFor="date">Date</label>
+                <input id="date" 
+                       type="date" 
+                       required
+                       defaultValue={date}
+                       ref={input => _date = input} />
 
-                    <div>
-                        <input id="powder" type="checkbox" ref="powder"  />
-                        <label htmlFor="powder">Powder Day</label>
-                    </div>
-                    <div>
-                        <input id="backcountry" type="checkbox" ref="backcountry" />
-                        <label htmlFor="backcountry">Backcountry</label>
-                    </div>
-                    <button>Add Day</button>
-                </form>
-            </div>
-        )
-    }
+                <div>
+                    <input id="powder" type="checkbox" ref={input => _powder = input}  />
+                    <label htmlFor="powder">Powder Day</label>
+                </div>
+                <div>
+                    <input id="backcountry" type="checkbox" ref={input => _backcountry = input} />
+                    <label htmlFor="backcountry">Backcountry</label>
+                </div>
+                <button>Add Day</button>
+            </form>
+        </div>
+    )
 }
 
 AddDayForm.defaultProps = {
@@ -56,10 +55,41 @@ AddDayForm.defaultProps = {
 
 export default AddDayForm
 
-// const AddDayForm = () => {
-//     return (
-//         <div>
-//             <h1>Add A Day</h1>
-//         </div>
-//     )
+// class AddDayForm extends Component {
+//     constructor (props) {
+//         super(props)
+//         this.submit = this.submit.bind(this)
+//     }
+//     render () {
+//         const {resort, date, powder, backcountry, onNewDay} = this.props 
+//         return (
+//             <div>
+//                 <form onSubmit={this.submit} className="add-day-form">
+//                     <label htmlFor="resort">Resort Name</label>
+//                     <input id="form" 
+//                            type="text" 
+//                            required
+//                            defaultValue={resort}
+//                            ref="resort" />
+
+//                     <label htmlFor="date">Date</label>
+//                     <input id="date" 
+//                            type="date" 
+//                            required
+//                            defaultValue={date}
+//                            ref="date" />
+
+//                     <div>
+//                         <input id="powder" type="checkbox" ref="powder"  />
+//                         <label htmlFor="powder">Powder Day</label>
+//                     </div>
+//                     <div>
+//                         <input id="backcountry" type="checkbox" ref="backcountry" />
+//                         <label htmlFor="backcountry">Backcountry</label>
+//                     </div>
+//                     <button>Add Day</button>
+//                 </form>
+//             </div>
+//         )
+//     }
 // }
