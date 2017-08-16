a72ad26130735482c6e7124071d5eb0be8eede33
Adham EL Banhawy <the.benhawy@gmail.com>
2017-08-15 23:10:23 -0500

    removed service worker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Whoops404 from './Whoops404';
-import registerServiceWorker from './registerServiceWorker';
 import { Router, Route, hashHistory } from 'react-router';
 
 // ReactDOM.render(<SkiDayCount backcountry={200} />, document.getElementById('root'));
@@ -16,4 +15,3 @@ ReactDOM.render(<Router history={hashHistory}>
                     <Route path="add-day" component={App}/>
                     <Route path="*" component={Whoops404}/>
                 </Router>, document.getElementById('root'));
-registerServiceWorker();
